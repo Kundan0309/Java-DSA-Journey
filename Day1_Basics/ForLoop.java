6a87f68744a263f8b3e43d322f64a0af609645aa
Kundan <kd.cod.1198@gmail.com>
2025-06-22 07:29:08 +0530

    ForLoop.java

---
+++ Day1_Basics/ForLoop.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class ForLoop {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        for(int x=0;x<=10;x++){
+            System.out.println(x);
+        }
+    }
+}
