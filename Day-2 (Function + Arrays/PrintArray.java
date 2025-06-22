0f15cdc8f2ec1024cf84c05597c3823d93787a44
Kundan <kd.cod.1198@gmail.com>
2025-06-23 02:05:13 +0530

    Successfully did Array Code

---
+++ Day-2 (Function + Arrays/PrintArray.java
@@ -0,0 +1,13 @@
+public class PrintArray {
+    public static void main (String[] args){
+    
+        //array declare
+        int[] marks= new int[3];
+        marks[0]=99;
+        marks[1]=98;
+
+        //array Print
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+    }
+}
