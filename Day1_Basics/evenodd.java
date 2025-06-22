83f60ed88afe61a63a491100e58fa330b21f5708
Kundan <kd.cod.1198@gmail.com>
2025-06-22 07:27:57 +0530

    evenodd.java

---
+++ Day1_Basics/evenodd.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class evenodd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        if(x%2==0){
+            System.out.println("No is Odd");
+        }
+        else{
+            System.out.println("No is Even");
+        }
+    }
+}
